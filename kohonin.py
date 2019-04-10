b4936fe9416af7fe37b0d2be28297baea9489c1b
sergey131997 <shuctrikc555@mail.ru>
2019-04-10 06:48:51 +0300

    refactor kohonin.py

--- kohonin.py
+++ kohonin.py
@@ -25,6 +25,7 @@ class Kohonin(object):
                                   summ for j in range(input_size)]
 
     def TrainKohonin(self, data, save_path, epoch=10):
+        ifs = open(save_path[-5:] + "_log.txt", "w")
         for t in range(epoch):
             err = 0
             pyplot.figure()
@@ -42,20 +43,24 @@ class Kohonin(object):
             err /= len(data)
 
             print("Epoch:{}\n{}".format(t, err))
+            ifs.write("Epoch:{}\n{}\n".format(t, err))
 
             for ex in data:
                 d = self.GetCloses(ex)
                 self.Update(d, ex, t)
+        ifs.close()
 
     def TrainClassifier(self, data, class_number, func, save_path, epoch):
         self.classes = []
         self.func = func
 
         b_error = 1000
+        ifs = open(save_path[-5:] + "_log.txt", "w")
 
         for i in range(epoch):
             cl, err = self.TrainClassifierIteration(data, class_number, func)
-            print ("Epoch: {}\n{}".format(i, err))
+            print("Epoch: {}\n{}".format(i, err))
+            ifs.write("Epoch: {}\n{}\n".format(i, err))
             if err < b_error:
                 b_error = err
                 self.classes = cl
@@ -72,6 +77,7 @@ class Kohonin(object):
                     pyplot.scatter(x_, y_)
                 pyplot.savefig(save_path[:-4] + "png")
                 pyplot.close()
+        ifs.close()
 
     def TrainClassifierIteration(self, data, class_number, func):
         classes_1 = []
@@ -101,6 +107,9 @@ class Kohonin(object):
             classes_2 = [[] for i in range(len(classes_1))]
 
             for i in range(len(classes)):
+                if len(classes[i]) == 0:
+                    classes_2[i] = classes_1[i]
+                    continue
                 classes_2[i] = [sum([j[0] for j in classes[i]]) / len(classes[i]),
                                 sum([j[1] for j in classes[i]]) / len(classes[i])]
             c = classes_1
@@ -177,7 +186,79 @@ class Kohonin(object):
         self.map = js["map"]
         self.classes = js["classes"]
         self.func = js["func"]
+        self.input_size = len(self.map[0][0])
+        self.width = len(self.map[0])
+        self.height = len(self.map)
 
     def Save(self, path):
-        json.dump({"map": self.map, "classes": self.classes,
+        json.dump({"map": self.map,
+                   "classes": self.classes,
                    "func": self.func}, open(path, "w"), indent=2)
+
+    def Siluet(self, x):
+        classes = []
+        z = []
+        for i in x:
+            for j in i:
+                z.append(j)
+        x = z
+        for i in x:
+            classes.append([self.GetCloses(i), self.Solve(i)])
+        s = []
+        for i in range(len(classes)):
+            a = [[] for j in range(len(self.classes))]
+            b = 10000000000000
+            for j in range(len(classes)):
+                if i == j:
+                    continue
+                d = self.GetP(classes[i][0], classes[j][0])
+                a[classes[j][1]].append(d)
+            for z in range(len(a)):
+                a[z] = sum(a[z]) / len(a[z])
+                if a[z] < b and z != classes[i][1]:
+                    b = a[z]
+            a = a[classes[i][1]]
+            s.append((b - a) / max(a, b))
+        return sum(s) / len(s)
+
+    def RandIndex(self, x):
+        classes = []
+        for i in range(len(x)):
+            for j in x[i]:
+                y = self.Solve(j)
+                classes.append([y, i])
+        a = 0
+        b = 0
+        for i in range(len(classes)):
+            for j in range(i + 1, len(classes)):
+                if i == j:
+                    continue
+                if classes[i][0] == classes[j][0] and classes[i][1] == classes[j][1]:
+                    a += 1
+                if classes[i][0] != classes[j][0] and classes[i][1] != classes[j][1]:
+                    b += 1
+
+        return (a + b) * 2 / (len(classes) * (len(classes) - 1))
+
+    def CalinskiHarabaszIndex(self, x):
+        classes = []
+        x_=0
+        y_=0
+        for i in range(len(x)):
+            for j in x[i]:
+                y = self.Solve(j)
+                gk = self.GetCloses(j)
+                x_ += gk[0]
+                y_ += gk[1]
+                classes.append([y, gk])
+
+        x_ /= len(classes)
+        y_ /= len(classes)
+
+        ssb = 0
+        ssw = 0
+
+        ssb = sum(Kohonin.Distance([x_, y_], self.classes[i]) * sum([1 for j in classes if j[0] == i]) for i in range(len(self.classes)))
+        ssw = sum(Kohonin.Distance(self.classes[i[0]], i[1]) for i in classes)
+
+        return (ssb / ssw) * (len(classes) - len(self.classes)) / (len(self.classes) - 1)
